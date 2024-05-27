434a8495bf2f13ea29c0217efd6a7eceadc5f2a7
ErnisKasumi <119933258+ErnisKasumi@users.noreply.github.com>
2024-05-27 12:14:12 +0200

    Added some section on homepage

--- js/swiper.js
+++ js/swiper.js
@@ -17,3 +17,20 @@ const swiper = new Swiper(".swiper", {
   speed: 400,
   spaceBetween: 100,
 });
+
+const swiper2 = new Swiper(".swiper2", {
+  slidesPerView: 3,
+  spaceBetween: 35,
+  slidesPerGroup: 1,
+  loop: true,
+  fade: true,
+  centerSlide: true,
+  grabCursor: true,
+  LoopfillGroupwithBlank: true,
+
+  autoplay: {
+    dealy: 5000,
+  },
+  speed: 400,
+  spaceBetween: 100,
+});
