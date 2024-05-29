36a00bf9b4a4f10f8cf2d1146ae23665342845b8
ErnisKasumi <119933258+ErnisKasumi@users.noreply.github.com>
2024-05-29 23:55:00 +0200

    Added animation preloader

--- js/script.js
+++ js/script.js
@@ -39,3 +39,10 @@ countersEl.forEach((counters) => {
     }
   }
 });
+
+const loader = document.querySelector(".loader");
+window.addEventListener("load", () => {
+  setTimeout(() => {
+    loader.style.display = "none";
+  }, 2000);
+});
